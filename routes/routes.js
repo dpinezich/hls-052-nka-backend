de9212633790ea29e7514068c2c4d2d44be02424
Alex Florescu <alu@panter.ch>
2019-02-25 01:44:08 +0100

    added email validation

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,7 @@ const stream = require('stream');
 const xlsx = require('node-xlsx').default;
 const mailer = require('../mailer');
 const TokenGenerator = require('uuid-token-generator');
+const emailValidator = require('email-validator');
 
 const fileName = 'db.json';
 
@@ -15,31 +16,33 @@ router.get('/', (req, res) => {
 });
 
 router.post('/save', (req, res) => {
-  fs.exists(fileName, (exists) => {
-    let allData = {
-      data: {}
-    };
-    const token = new TokenGenerator(256, TokenGenerator.BASE62).generate();
-    if (exists) {
-      fs.readFile(fileName, (err, data) => {
-        if (data) {
-          allData = JSON.parse(data);
-        }
-        allData[token] = req.body;
-        allData[token]['validated'] = 0;
-        fs.writeFile(fileName, JSON.stringify(allData), (error) => {});
+  const email = req.body.email;
+  if (email && emailValidator.validate(email)) {
+    fs.exists(fileName, (exists) => {
+      const token = new TokenGenerator(256, TokenGenerator.BASE62).generate();
+      let allData = {};
+      if (exists) {
+        fs.readFile(fileName, (err, data) => {
+          if (data) {
+            allData = JSON.parse(data);
+          }
+          saveData(allData, req.body, token);
+        });
+      } else {
+        saveData(allData, req.body, token);
+      }
+      const url = req.secure ? 'https://' : 'http://' + req.headers.host + '/validate/' + token;
+      mailer.sendConfirmationMail(req.body.email, req.body.name, url);
+      res.json({
+        msg: 'done'
       });
-    } else {
-      allData[token] = req.body;
-      allData[token]['validated'] = 0;
-      fs.writeFile(fileName, JSON.stringify(allData), (error) => {});
-    }
-    const url = req.secure ? 'https://' : 'http://' + req.headers.host + '/validate/' + token;
-    mailer.sendConfirmationMail(req.body.email, req.body.name, url);
-  });
-  res.json({
-    msg: 'done'
-  });
+    });
+  } else {
+    res.status(422);
+    res.json({
+      msg: 'invalid email'
+    });
+  }
 });
 
 router.get('/validate/:token?', (req, res) => {
@@ -116,5 +119,15 @@ router.get('/reset-db', (req, res) => {
     msg: 'done'
   });
 });
+
+function saveData(dataFromFile, clientData, token) {
+  dataFromFile[token] = clientData;
+  dataFromFile[token]['validated'] = 0;
+  fs.writeFile(fileName, JSON.stringify(dataFromFile), (error) => {});
+}
+
+function sendEmail({ name, email, url }) {
+  
+}
 
 module.exports = router;
